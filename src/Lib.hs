d4443ad18e0ffc60b4cf542ab84f0ab64f88b831
Nobuo Yamashita <nobsun@gmail.com>
2024-06-18 20:41:21 +0900

    add LANGUAGE pragma for using as an template module

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,22 @@
+-- # Lib 雛形モジュール
+-- ## 言語拡張と`module`宣言の雛形
+{-# LANGUAGE GHC2021 #-}
+{-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE LexicalNegation #-}
+{-# LANGUAGE LambdaCase, MultiWayIf #-}
+{-# LANGUAGE NPlusKPatterns #-}
+{-# LANGUAGE DataKinds, PolyKinds, NoStarIsType, TypeFamilyDependencies #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedRecordDot, NoFieldSelectors, DuplicateRecordFields #-}
 module Lib
     ( someFunc
     ) where
 
+{- |
+「なんか関数」を標準出力に印字する
+>>> someFunc
+なんか関数
+-}
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn "なんか関数"
