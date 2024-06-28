d34da94cc03c360c436e582d05f92819523f4395
cutsea110 <cutsea110@gmail.com>
2024-06-29 03:15:44 +0900

    eval

--- src/Eval.hs
+++ src/Eval.hs
@@ -12,6 +12,7 @@ eval e = case e of
   EInt i  -> Right e
   EStr s  -> Right e
   EUnary op e -> unary op e
+  EBinary op e1 e2 -> binary op e1 e2
 
 unary :: UOp -> Expr -> Either String Expr
 unary Not e = case e of
@@ -29,3 +30,48 @@ unary StrToInt e = case e of
 unary IntToStr e = case e of
     EInt i -> Right $ EStr $ cultToHuman $ encodeBase94 i
     _      -> Left "IntToStr applied to non-integer"
+
+binary :: BinOp -> Expr -> Expr -> Either String Expr
+binary Add e1 e2 = case (e1, e2) of
+    (EInt i1, EInt i2) -> Right $ EInt $ i1 + i2
+    _                  -> Left "Add applied to non-integers"
+binary Sub e1 e2 = case (e1, e2) of
+    (EInt i1, EInt i2) -> Right $ EInt $ i1 - i2
+    _                  -> Left "Sub applied to non-integers"
+binary Mult e1 e2 = case (e1, e2) of
+    (EInt i1, EInt i2) -> Right $ EInt $ i1 * i2
+    _                  -> Left "Mult applied to non-integers"
+binary Div e1 e2 = case (e1, e2) of
+    (EInt i1, EInt i2) -> Right $ EInt $ i1 `div` i2
+    _                  -> Left "Div applied to non-integers"
+binary Mod e1 e2 = case (e1, e2) of
+    (EInt i1, EInt i2) -> Right $ EInt $ i1 `mod` i2
+    _                  -> Left "Mod applied to non-integers"
+binary Lt e1 e2 = case (e1, e2) of
+    (EInt i1, EInt i2) -> Right $ EBool $ i1 < i2
+    _                  -> Left "Lt applied to non-integers"
+binary Gt e1 e2 = case (e1, e2) of
+    (EInt i1, EInt i2) -> Right $ EBool $ i1 > i2
+    _                  -> Left "Gt applied to non-integers"
+binary Eql e1 e2 = case (e1, e2) of
+    (EInt i1, EInt i2) -> Right $ EBool $ i1 == i2
+    _                  -> Left "Eql applied to non-integers"
+binary Or e1 e2 = case (e1, e2) of
+    (EBool b1, EBool b2) -> Right $ EBool $ b1 || b2
+    _                    -> Left "Or applied to non-booleans"
+binary And e1 e2 = case (e1, e2) of
+    (EBool b1, EBool b2) -> Right $ EBool $ b1 && b2
+    _                    -> Left "And applied to non-booleans"
+binary Concat e1 e2 = case (e1, e2) of
+    (EStr s1, EStr s2) -> Right $ EStr $ BS.append s1 s2
+    _                  -> Left "Concat applied to non-strings"
+binary Take e1 e2 = case (e1, e2) of
+    (EInt i, EStr s) -> Right $ EStr $ BS.take (fromIntegral i) s
+    _                -> Left "Take applied to non-integer and non-string"
+binary Drop e1 e2 = case (e1, e2) of
+    (EInt i, EStr s) -> Right $ EStr $ BS.drop (fromIntegral i) s
+    _                -> Left "Drop applied to non-integer and non-string"
+binary Apply e1 e2 = case e1 of
+    ELambda v e -> Right $ e
+    _           -> Left "Apply applied to non-lambda"
+
