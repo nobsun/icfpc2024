e687d800eec678e673fefcdd9f4ef4693cf1d504
cutsea110 <cutsea110@gmail.com>
2024-06-28 20:59:01 +0900

    test

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,6 +13,9 @@ module Lib
     ( someFunc
     ) where
 
+import Data.Char (chr, ord)
+import Data.Maybe (mapMaybe)
+
 {- |
 「なんか関数」を標準出力に印字する
 >>> someFunc
@@ -20,3 +23,24 @@ module Lib
 -}
 someFunc :: IO ()
 someFunc = putStrLn "なんか関数"
+
+{- |
+>>> int "/6"
+1337
+-}
+int :: String -> Int
+int = foldl (\acc c -> acc * 94 + ord c - ord '!') 0
+
+-- str :: String -> String
+str = mapMaybe $ flip lookup dict
+
+dict = zip cultCode humanReadable
+  where
+    cultCode = map chr [33..126]
+    humanReadable = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n"
+
+-- hello world
+test = "B%,,/}Q/2,$_"
+
+-- curl -X POST -H 'Authorization: Bearer 39572a1c-b861-4e57-8405-b9fda4f8cec3' https://boundvariable.space/communicate
+communicate = "J!23%}%22/2n}O.%80%#4%$}?I@"
