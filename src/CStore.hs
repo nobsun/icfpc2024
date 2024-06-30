19a3f4c6ee766cae336d9c1bc9ffafdb21df5049
Kei Hibino <ex8k.hibino@gmail.com>
2024-06-30 22:13:34 +0900

    CStore, storeSolve helper

--- src/CStore.hs
+++ src/CStore.hs
@@ -103,6 +103,19 @@ allList =
 
 ---
 
+writeExpr :: String -> Expr -> IO ()
+writeExpr path expr = do
+  case expr of
+    (EStr s)  -> do
+      putStrLn $ "writing top-level EStr string " ++ path ++ " ..."
+      B8.writeFile path s
+    _         -> do
+      putStrLn $ "writing " ++ path ++ " ..."
+      writeFile path (show expr <> "\n")
+  putStrLn "done."
+
+---
+
 storeLangFile :: String -> FilePath -> IO ()
 storeLangFile pname path = do
   putStrLn $ "geting " ++ pname ++ " ..."
@@ -135,15 +148,8 @@ storeLangExpr pname = do
   let epath = problemsDir </> pname <.> etypeName EExpr
   putStrLn $ "loading " ++ lpath ++ " ..."
   eexpr <- Parser.parseExpr_ <$> B8.readFile lpath
-  let write (EStr s) = do
-        putStrLn $ "writing top-level EStr string " ++ epath ++ " ..."
-        B8.writeFile epath s
-      write expr = do
-        putStrLn $ "writing " ++ epath ++ " ..."
-        writeFile epath (show expr <> "\n")
-        putStrLn "done."
-      skip e = putStrLn $ "parse error: " ++ e ++ ": skipping"
-  either skip write eexpr
+  let skip e = putStrLn $ "parse error: " ++ e ++ ": skipping"
+  either skip (writeExpr epath) eexpr
 
 loadLangExpr :: String -> IO Expr
 loadLangExpr pname = do
@@ -166,6 +172,14 @@ storePPR pname = do
       let path = prettyDir </> pname <.> etypeName etype
       putStrLn $ "writing " ++ path ++ " ..."
       writeFile' path (ppr expr)
+
+storeSolve :: String -> String -> String -> IO ()
+storeSolve pname solution solName = do
+  rexpr <- Comm.solve pname solution
+  let spath = solName <.> "txt"
+      rpath = solName <> "_" <> "result" <.> "txt"
+  writeFile spath (solution <> "\n")
+  writeExpr rpath rexpr
 
 ---
 
